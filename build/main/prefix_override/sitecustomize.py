c93701b1e0801e053a36a152c3343159be87a28c
Tu Nombre <you@example.com>
2025-11-14 22:33:47 -0600

    reiniciando repositorio local

--- build/main/prefix_override/sitecustomize.py
+++ build/main/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/geovani/Ne2BU/src/install/main'
+    sys.prefix = sys.exec_prefix = '/home/geovani/Ne2BU/install/main'
